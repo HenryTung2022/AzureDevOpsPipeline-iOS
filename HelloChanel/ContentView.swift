07dc83c188e8309f93fae2f530e8d0f35ae032f8
Chi Yung Tung <cscyhenry@gmail.com>
2022-11-12 09:39:12 +0800

    Initial Commit

---
+++ HelloChanel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloChanel
+//
+//  Created by Chi Yung Tung on 12/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloChanel/HelloChanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloChanelApp.swift
+//  HelloChanel
+//
+//  Created by Chi Yung Tung on 12/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloChanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
